0137a0cdedabad5e379a08ace0cd42adcb9eab7f
nhamrin <104506300+nhamrin@users.noreply.github.com>
2025-02-11 21:22:22 +0100

    Rearrange files
    Rearrange files to suit the course's format

--- 1.2/app/src/main/java/com/example/assg12/MainActivity.kt
+++ 1.2/app/src/main/java/com/example/assg12/MainActivity.kt
@@ -7,7 +7,6 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
