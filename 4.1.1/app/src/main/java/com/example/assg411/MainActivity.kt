3106bc38060fe73c84ce597a1ce4fb622ccbe555
nhamrin <104506300+nhamrin@users.noreply.github.com>
2025-03-17 19:25:11 +0100

    Remove unused import
    Remove unused preview import

--- 4.1.1/app/src/main/java/com/example/assg411/MainActivity.kt
+++ 4.1.1/app/src/main/java/com/example/assg411/MainActivity.kt
@@ -14,7 +14,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.tooling.preview.Preview
 
 /**
  * The class MainActivity
